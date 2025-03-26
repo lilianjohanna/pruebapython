1079364ded8095d6669db02fde9245e7a2d86e02
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-03-26 21:59:11 +0000

    ...

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@ import requests
 import urllib.parse
 
 parametro='1'
-parametro2='4'
+parametro2='5'
 URL = 'https://jsonplaceholder.typicode.com/comments?postId='+parametro+'&id='+parametro2
 response = requests.get(URL, parametro)
 
