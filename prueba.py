77d383bc632d52ee1c9a71101c9f2879d3a2656b
lilianjohanna <61803904+lilianjohanna@users.noreply.github.com>
2025-03-26 21:50:42 +0000

    ...

--- prueba.py
+++ prueba.py
@@ -1,7 +1,7 @@
 import requests
 import urllib.parse
 
-parametro = 'Kamren'
+parametro = '3'
 URL = 'https://jsonplaceholder.typicode.com/users/'+parametro
 response = requests.get(URL, parametro)
 
